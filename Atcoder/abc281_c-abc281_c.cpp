a713b7c8f87ffcadf47608886a350a5ca0f76703
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-21 02:01:35 +0600

    Committing files

---
+++ Atcoder/abc281_c-abc281_c.cpp
@@ -0,0 +1,62 @@
+
+//15jan2023(10:02PM)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+
+int main()
+{
+    faster;
+    ll n,m,t,i,j,k,x,y,z,a,b,sum=0,c,cnt=0,ans=0;
+    string s,s1,s2,s3;
+    char c1,c2;
+    cin>>n>>k;
+    vcd(vc);
+    fl(i,0,n)
+    {
+        cin>>a;
+        sum+=a;
+        vc.pb(sum);
+
+    }
+    k=k%sum;
+    fl(i,0,n)
+    {
+        if(k<=vc[i])
+           {
+               if(i==0)
+                {
+                    cout<<i+1<<" "<<k<<endl;
+                    break;
+                }
+                else
+                {
+                    cout<<i+1<<" "<<k-vc[i-1]<<endl;break;
+                }
+           }
+    }
+
+
+
+
+  //main();
+}
