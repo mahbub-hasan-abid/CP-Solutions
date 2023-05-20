4a4b1253a934062345b994c0ed7a92a77d5f1fd4
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-21 02:05:36 +0600

    Committing files

---
+++ 549A-Face Detection.cpp
@@ -0,0 +1,62 @@
+ 
+ 
+//27march2023(8:09PM)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+ 
+int main()
+{
+    faster;
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0,sum=0;
+    string s,s1,s2,s3;
+ 
+    cin>>n>>m;
+    char ch[n][m];
+    fl(i,0,n)
+    {
+        fl(j,0,m)
+        {
+            cin>>ch[i][j];
+        }
+    }
+ fl(i,0,n-1)
+    {
+        fl(j,0,m-1)
+        {
+            map<char,ll>mp;
+            mp[ch[i][j]]++;
+            mp[ch[i][j+1]]++;
+            mp[ch[i+1][j]]++;
+            mp[ch[i+1][j+1]]++;
+            if(mp['f']>0&&mp['a']>0&&mp['c']>0&&mp['e']>0)cnt++;
+ 
+        }
+ 
+    }
+ 
+    cout<<cnt<<endl;
+ 
+ 
+ 
+ // main();
+}
