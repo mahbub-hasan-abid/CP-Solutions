d6b25b98954275a14c9232032d77c8b71e191878
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-21 02:01:56 +0600

    Committing files

---
+++ 1833A-Musical Puzzle.cpp
@@ -0,0 +1,57 @@
+ 
+//15jan2023(10:02PM)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define p-1 cout<<"-1"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+void sol()
+{
+    ll i,n;
+    string s;
+    cin>>n>>s;
+    set<string>st;
+    fl(i,0,s.size()-1)
+    {
+        string s1;
+        s1=s.substr(i,2);
+        st.insert(s1);
+    }
+cout<<st.size()<<endl;
+ 
+ 
+}
+ 
+int main()
+{
+    faster;
+    ll tc;
+    cin>>tc;
+    while(tc--)
+        sol();
+ 
+ 
+ 
+ 
+ 
+ 
+  //main();
+}
