25a3057108fe30093ac40df036ebcd94bf2091a9
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-21 02:01:54 +0600

    Committing files

---
+++ 1833C-Vlad Building Beautiful Array.cpp
@@ -0,0 +1,70 @@
+ 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define p-1 cout<<"-1"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+void sol()
+{
+    ll n,i,a,odd=0,even=0;
+    vcd(vc);
+    cin>>n;
+    fl(i,0,n)
+    {
+ 
+        cin>>a;
+        if(a%2==0)even++;
+        else odd++;
+        vc.pb(a);
+ 
+    }
+    if(even==0||odd==0)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+        {
+            ll mn=*min_element(all(vc));
+            if(mn%2==0)cout<<"NO"<<endl;
+            else
+                cout<<"YES"<<endl;
+ 
+        }
+ 
+ 
+ 
+}
+ 
+int main()
+{
+    faster;
+    ll tc;
+    cin>>tc;
+    while(tc--)
+        sol();
+ 
+ 
+ 
+ 
+ 
+ 
+  //main();
+}
