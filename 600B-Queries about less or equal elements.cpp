322a220755aa20fc13f34d26e8d3ec4542d607d4
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-21 02:02:11 +0600

    Committing files

---
+++ 600B-Queries about less or equal elements.cpp
@@ -0,0 +1,47 @@
+ 
+//15may2023(7:21PM)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+ 
+int main()
+{
+    faster;
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    string s,s1,s2,s3;
+    cin>>n>>m;
+    vcd(va);
+    vcd(vb);
+    getintovc(va,n);
+    getintovc(vb,m);
+    sort(all(va));
+    fl(i,0,vb.size())
+    {
+         ans=upper_bound(va.begin(),va.end(),vb[i])-va.begin();
+         cout<<ans<<" ";
+    }
+ 
+ 
+ 
+ 
+  //main();
+}
