e8bd140a1d3c664a52eb3af3e164e5ab35b14c20
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-21 02:02:20 +0600

    Committing files

---
+++ 1509B-TMT Document.cpp
@@ -0,0 +1,73 @@
+//10may2023(8:46PM)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define p-1 cout<<"-1"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+int sol()
+{
+    ll n,i;
+    string s;
+    cin>>n>>s;
+    ll noT=count(s.begin(),s.end(),'T');
+    ll noM=count(s.begin(),s.end(),'M');
+    vcd(m);vcd(t);
+    fl(i,0,n)
+    {
+        if(s[i]=='T')
+            t.pb(i);
+        else
+            m.pb(i);
+    }
+    if(noT!=(2*noM))
+    {
+        no
+        return 0;
+    }
+     fl(i,0,noM)
+     {
+         if(t[i]>m[i]||m[i]>t[i+noM])
+         {
+             no
+             return 0;
+         }
+     }
+ 
+ 
+yes
+return 0;
+}
+ 
+int main()
+{
+    faster;
+    ll tc;
+    cin>>tc;
+    while(tc--)
+        sol();
+ 
+ 
+ 
+ 
+ 
+ 
+  //main();
+}
