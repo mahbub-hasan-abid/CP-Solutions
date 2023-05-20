55dff1e8000c8dc3e905df23bd1b660976a1e644
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-21 02:04:29 +0600

    Committing files

---
+++ 1144A-Diverse Strings.cpp
@@ -0,0 +1,68 @@
+//15jan2023(10:02PM)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define p-1 cout<<"-1"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+int sol()
+{
+    string s;
+    ll i;
+    cin>>s;
+    sort(s.begin(),s.end());
+    if(s.size()==1)
+    {
+        yes;
+        return 0;
+    }
+    else
+    {
+        fl(i,1,s.size())
+    {
+        if(s[i]-s[i-1]!=1)
+        {
+            no;
+            return 0;
+        }
+    }
+    }
+    yes;
+ 
+return 0;
+ 
+ 
+}
+ 
+int main()
+{
+    faster;
+    ll tc;
+    cin>>tc;
+    while(tc--)
+        sol();
+ 
+ 
+ 
+ 
+ 
+ 
+  //main();
+}
