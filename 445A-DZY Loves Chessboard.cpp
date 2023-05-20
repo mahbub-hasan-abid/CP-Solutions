36724f88384f4987b30fc0a19b11c83933ca84cb
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-21 02:06:30 +0600

    Committing files

---
+++ 445A-DZY Loves Chessboard.cpp
@@ -0,0 +1,53 @@
+ 
+//17march2023(10:21PM)
+//17march2023(11:49PM)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+ 
+int main()
+{
+    faster;
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    string s,s1,s2,s3;
+    char aa;
+    char bb='B',ww='W';
+    cin>>n>>m;
+    vector<char>vc[n];
+    fl(i,0,n)
+    {
+        fl(j,0,m)
+        {
+            cin>>aa;
+           if(aa=='-')
+            cout<<aa;
+           else
+           {
+               if((i+j)%2==0)
+                cout<<'W';
+               else cout<<'B';
+           }
+        }cout<<endl;
+ 
+    }
+    //main();
+}
