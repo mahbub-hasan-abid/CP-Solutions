c114052fac35465f9cedf04c45ff39e220541adf
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-21 02:02:34 +0600

    Committing files

---
+++ 1557B-Moamen and k-subarrays.cpp
@@ -0,0 +1,64 @@
+ 
+//5may2023(9:55PM)
+//5may2023(11:04PM)
+//500 th problem
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define p-1 cout<<"-1"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+void sol()
+{
+    ll i,n,m,a,cnt=1;
+    cin>>n>>m;
+   vector<pair<ll,ll>>vc;
+   fl(i,0,n)
+   {
+       cin>>a;
+       vc.pb({a,i});
+   }
+   sort(all(vc));
+   fl(i,0,n-1)
+       if(vc[i+1].second-vc[i].second!=1)cnt++;
+ 
+    if(cnt<=m)
+         yes
+         else no
+ 
+ 
+ 
+}
+ 
+int main()
+{
+    faster;
+    ll tc;
+    cin>>tc;
+    while(tc--)
+        sol();
+ 
+ 
+ 
+ 
+ 
+ 
+  //main();
+}
