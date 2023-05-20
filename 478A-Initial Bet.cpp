90e8da4c5f33c7d752f2beb04d460f8e17775ce8
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-21 02:04:57 +0600

    Committing files

---
+++ 478A-Initial Bet.cpp
@@ -0,0 +1,48 @@
+ 
+//3april2023(8:21PM)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}n=vc.size();}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) {for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";cout<<endl;}
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+ 
+int main()
+{
+    faster;
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    string s,s1,s2,s3;
+    cin>>n>>m>>i>>j>>k;
+    ll sum=(n+m+i+j+k);
+    float div=5,xx=sum/div;
+    ans=xx;
+   // cout<<xx<<" "<<sum<<endl;
+   // if((xx*10)==(ans*10))
+   if(floor(xx)==xx&&sum!=0)
+    {
+      cout<<xx<<endl;
+    }
+    else cout<<-1<<endl;
+ 
+ 
+ 
+ 
+ 
+  //main();
+}
