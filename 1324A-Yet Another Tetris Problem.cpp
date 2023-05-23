fa9f7a7f6ce51624efd9d3bdc08278e0abb70517
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-23 19:56:35 +0600

    Committing files

---
+++ 1324A-Yet Another Tetris Problem.cpp
@@ -0,0 +1,54 @@
+ 
+//5march2023(10:32PM)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define p-1 cout<<"-1"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+void sol()
+{
+    ll n,i,a,evn=0,odd=0;
+    cin>>n;
+    fl(i,0,n)
+    {
+        cin>>a;
+        if(a%2==0)evn++;
+        else odd++;
+    }
+    if(evn==0&&odd>0||evn>0&&odd==0)yes
+        else no
+        }
+ 
+int main()
+{
+    faster;
+    ll tc;
+    cin>>tc;
+    while(tc--)
+        sol();
+ 
+ 
+ 
+ 
+ 
+ 
+    //main();
+}
