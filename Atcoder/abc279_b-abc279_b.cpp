fdc50f8bb63a4f055094ac6c19f67450ad344024
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-23 19:51:06 +0600

    Committing files

---
+++ Atcoder/abc279_b-abc279_b.cpp
@@ -0,0 +1,50 @@
+
+//15jan2023(10:02PM)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+
+int main()
+{
+    faster;
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    string s,s1,s2,s3;
+    cin>>s>>s1;
+    fl(i,0,s.size())
+    {
+        if(s[i]==s1[0])
+        {
+            s2=s.substr(i,s1.size());
+           if(s1==s2)
+           {
+               cout<<"Yes"<<endl;
+               return 0;
+           }
+        }
+    }
+    cout<<"No"<<endl;
+
+
+
+
+  //main();
+}
