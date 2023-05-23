3284667f898f2fac546bb918b41e50b8a1d2ae16
Mahbub Hasan Abid <133025212+mahbub-hasan-abid@users.noreply.github.com>
2023-05-23 19:51:36 +0600

    Committing files

---
+++ 546B-Soldier and Badges.cpp
@@ -0,0 +1,51 @@
+ 
+//23may2023(1:02AM)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for( i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define vcd(v) vector<ll> (v);
+#define vcsort(vc) sort(v.begin(), v.end());
+#define vcsortg(vc) sort(v.begin(), v.end(), greater<int>());
+#define getintovc(vc,n) { while(n--) {long long yyy; cin>>yyy; vc.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(vc) for(int i=0; i<vc.size(); i++) cout<<vc[i]<<" ";
+#define ff first
+#define ss second
+#define faster; {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+ 
+int main()
+{
+    faster;
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    string s,s1,s2,s3;
+    cin>>n;
+    vcd(vc);
+    getintovc(vc,n);
+    sort(all(vc));
+    ll sum=0;
+    fl(i,1,vc.size())
+    {
+        if(vc[i-1]>=vc[i])
+        {
+            sum+=vc[i-1]+1-vc[i];
+            vc[i]=vc[i-1]+1;
+        }
+    }
+    cout<<sum<<endl;
+ 
+ 
+ 
+ 
+ 
+  //main();
+}
